e17fbe33a5124642ed4242cf61ca74b5bb4cb268
rahuly <rahulyadavv2011421@gmail.com>
2024-06-17 20:24:40 +0530

    initial commit

---
+++ jenkin.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class jenkin{
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Addition of two a=5 and b=12 is : " + (5+12));
+        System.out.println(new Date());
+    }
+}
