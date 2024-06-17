4ab01f7a5633b54bb91cfe6cf76be369e254e98c
rahuly <rahulyadavv2011421@gmail.com>
2024-06-17 20:41:17 +0530

    added Student class to check jenkin is really triggering the automation

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating object fot Student class");
+    }
+    public static void sayHello(){
+        System.out.println("Virat you beauty");
+    }
+    
+}
--- jenkin.java
+++ jenkin.java
@@ -1,9 +1,12 @@
 import java.util.Date;
 
-public class jenkin{
+public class Jenkin{
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("Addition of two a=5 and b=12 is : " + (5+12));
         System.out.println(new Date());
+        Student student=new Student();
+        student.sayHello();
+        System.out.println("Done with testing of first project of jenkins..................");
     }
 }
